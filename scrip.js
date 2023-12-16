da39b359d7bb4b9e8b9f71090ac55ac9315d0e76
Lada Jakab <132440338+Lada-Montcorbier@users.noreply.github.com>
2023-12-16 12:48:48 +0100

    Finalising game messages and logic
    Finalising game messages and logic

--- scrip.js
+++ scrip.js
@@ -144,6 +144,34 @@ window.addEventListener('load', function () {
       for (let i = 0; i < this.game.ammo; i++) {
         context.fillRect(20 + 5 * i, 50, 3, 20);
       }
+      // timer
+      const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+      context.fillText('Timer: ' + formattedTime, 20, 100);
+      // game over messages
+      if (this.game.gameOver) {
+        context.textAlign = 'center';
+        let message1;
+        let message2;
+        if (this.game.score >= this.game.winningScore) {
+          message1 = 'You win!';
+          message2 = 'Well done!';
+        } else {
+          message1 = 'You lose!';
+          message2 = 'Better luck next time!';
+        }
+        context.font = '50px ' + this.fontFamily;
+        context.fillText(
+          message1,
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 40,
+        );
+        context.font = '25px ' + this.fontFamily;
+        context.fillText(
+          message2,
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 40,
+        );
+      }
       context.restore();
     }
   }
@@ -166,8 +194,12 @@ window.addEventListener('load', function () {
       this.gameOver = false;
       this.score = 0;
       this.winningScore = 10;
+      this.gameTime = 0;
+      this.timeLimit = 5000;
     }
     update(deltaTime) {
+      if (!this.gameOver) this.gameTime += deltaTime;
+      if (this.gameTime > this.timeLimit) this.gameOver = true;
       this.player.update();
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
@@ -186,7 +218,7 @@ window.addEventListener('load', function () {
             projectile.markedForDeletion = true;
             if (enemy.lives <= 0) {
               enemy.markedForDeletion = true;
-              this.score += enemy.score;
+              if (!this.gameOver) this.score += enemy.score;
               if (this.score >= this.winningScore) this.gameOver = true;
             }
           }
